026b6f1358cbb2f58286961591ea99c0b9c1cf8a
dinma773-3 <godsfavourcukwudi21@gmail.com>
2023-10-30 07:58:56 -0700

    file updated

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
-"""
-"""
+"""app.py to connect to API"""
+import os
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, jsonify, make_response, render_template, url_for
-from flask_cors import CORS, cross_origin
+from flask import Flask, Blueprint, jsonify, make_response
+from flask_cors import CORS
+
 
 app = Flask(__name__)
-
-# flask server environmental setup
-host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-port = os.getenv('HBNB_API_PORT', 5000)
-
-# create a CORS instance
-cors = CORS(app, resources={r'/*': {'origins': host}})
-
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    # method to handle @app.teardown_appcontext that calls storage.close()
+def teardown_appcontext(code):
+    """teardown_appcontext"""
     storage.close()
 
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 if __name__ == "__main__":
-    # initializes global error handling
-    setup_global_errors()
-    # start Flask app
-    app.run(host=host, port=port)
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
